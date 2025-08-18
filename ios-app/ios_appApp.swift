0324cf31856d3a2ae29bccb9cb21885d04a07385
Jørgen Henriksen <jorgenwhen@gmail.com>
2025-08-18 17:16:41 +0200

    Initial Commit

---
+++ ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by Jørgen Henriksen on 18/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-appTests/ios_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appTests.swift
+//  ios-appTests
+//
+//  Created by Jørgen Henriksen on 18/08/2025.
+//
+
+import Testing
+@testable import ios_app
+
+struct ios_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
